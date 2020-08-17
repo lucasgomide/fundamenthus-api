9dfd391dd0b0c99e2f1430935ab49cc6601d40eb
lucasgomide <lucaslg200@gmail.com>
2020-08-17 17:53:20 -0300

    Adding worker to collect data

---
+++ app/workers/collect_worker.rb
@@ -0,0 +1,12 @@
+class CollectWorker < ApplicationJob
+  queue_as :source
+
+  def perform
+    input = { source_names: ['b3', 'status_invest'], storage_names: ['google_sheets'] }
+    operation.call(input)
+  end
+
+  def operation
+    @operation ||= SourceOperation.new
+  end
+end
---
+++ spec/workers/collect_worker_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+RSpec.describe CollectWorker, type: :worker do
+  subject(:worker) { described_class.new(operation: operation) }
+  let(:operation) { instance_spy(SourceOperation) }
+
+  context '.perform' do
+    subject(:perform) { worker.perform }
+
+    before do
+      allow(worker).to receive(:operation).and_return(operation)
+    end
+
+    it do
+      perform
+      expect(operation).to have_received(:call).with(
+        source_names: ['b3', 'status_invest'],
+        storage_names: ['google_sheets']
+      )
+    end
+  end
+end
